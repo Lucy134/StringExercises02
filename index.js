828ba4c3931823a19626cea9ec89505be12c78ea
Lucy <>
2021-05-24 23:43:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let num = 1001.45;
+let numStr = num.toString();
+//Returns 'undefined'.
+if(numStr.search('.') != -1){
+let x = numStr.indexOf('.');
+console.log(numStr.length-(x+1));
+}
+else{
+  console.log(numStr.length);
+}
+
+
+//Use type conversion to print the length (number of digits) of an integer.
+
+
+
+
+//Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+
+
+
+
+//Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
